599d9e823d437b82cc8c6695d6ed39c94af752dc
Nicolas Hillegeer <nicolas@hillegeer.com>
2015-08-29 23:21:11 +0200

    v0.0.3

--- upload.sh
+++ upload.sh
@@ -1,12 +1,30 @@
 #!/bin/bash
 
-PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-DIR=$(basename $(PATH))
-LAST_TAG=$(git describe --abbrev=0 --tags)
+# set -e
+set -u
 
-github-release release \
+SCRIPT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+DIR=$(basename $SCRIPT_PATH)
+LAST_TAG=$(git describe --abbrev=0 --tags)
+VERBOSE=
+
+github-release $VERBOSE release \
     --user aktau \
     --repo $DIR  \
     --tag $LAST_TAG \
-    --name "the wolf of source street" \
+    --name "the dog of source street" \
     --description "Not a movie, contrary to popular opinion. Still, my first release!" \
+
+github-release $VERBOSE edit \
+    --user aktau \
+    --repo $DIR  \
+    --tag $LAST_TAG \
+    --name "the cow of cows" \
+    --description "Just a regular edit, nothing to see here folks" \
+
+github-release $VERBOSE edit \
+    --user aktau \
+    --repo $DIR  \
+    --tag "v0.0.1" \
+    --name "this should not work" \
+    --description "at all..." \
